e077a68ba5a795c62581896dc43351191f79be94
Mia Bojcic <mia@laplacian.hr>
2019-03-18 21:50:15 +0100

    Settings Page Layout.

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {Settings} from './settings';
 
 @Component({
   selector: 'tfg-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
+
+
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(value: Settings) {
+    console.log(value.personal);
+  }
 }
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
-
+import { MatButtonModule, MatFormFieldModule, MatInputModule}  from '@angular/material';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [SettingsComponent],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
   ]
 })
-export class SettingsModule { }
+export class SettingsModule {
+
+}
