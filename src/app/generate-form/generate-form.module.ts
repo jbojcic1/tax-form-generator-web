57b5f3fc51094aafd1db241fd50cac5d1e58deb0
Mia Bojcic <mia@laplacian.hr>
2019-03-19 16:52:21 +0100

    Generate-form layout.

--- src/app/generate-form/generate-form.module.ts
+++ src/app/generate-form/generate-form.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { GenerateFormRoutingModule } from './generate-form-routing.module';
 import { GenerateFormComponent } from './generate-form.component';
+import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [GenerateFormComponent],
   imports: [
     CommonModule,
-    GenerateFormRoutingModule
+    GenerateFormRoutingModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule
   ]
 })
 export class GenerateFormModule { }
