8d159be66a9800cfb3259aacb56c6227f268dd05
Petar Milic <pmilic@laplacian.hr>
2020-05-03 18:21:51 +0200

    removed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://backend:5000'
+  backendUrl: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://backend:5000'
+  backendUrl: ''
 };
 
 /*
